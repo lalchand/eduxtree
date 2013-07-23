a5c0ed72cb277f2ecb22b09d9df2e385f1f3d6df
lalchand <lcbisu@gmail.com>
2013-07-23 16:50:56 +0530

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Eduxtree::Application.config.secret_key_base = 'c90aa20589e545ad29569eed23603c25eb6c31eaf1d21795ee327acdb233361b6f85c129aa98e1e10ad1a83780e0a7c4bd2e2a552628c93f4b35c7db85fd7bdd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Eduxtree::Application.config.session_store :cookie_store, key: '_eduxtree_session'
