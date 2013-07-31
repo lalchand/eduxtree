30125bc87e92d812941d12d59ccf6ae70d009240
lalchand <lcbisu@gmail.com>
2013-07-31 18:54:10 +0530

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Eduxtree::Application.routes.draw do
-  get "users/new"
+  resources :users
   root  'static_pages#home'
   match '/signup',  to: 'users#new',            via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
@@ -7,7 +7,7 @@ Eduxtree::Application.routes.draw do
   match '/contact', to: 'static_pages#contact', via: 'get'
   resources :microposts
 
-  resources :users
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
